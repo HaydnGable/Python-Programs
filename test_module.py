c1237d763593c29c18f92e01b154ef1dab52cad6
Haydn Gable <gablehaydn7@gmail.com>
2026-03-23 11:09:45 -0400

    unittest tests written and completed

---
+++ test_module.py
@@ -0,0 +1,21 @@
+import unittest
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+    
+    def test_tuple_int(self):
+        data = (4, 5, 6)
+        result = sum(data)
+        self.assertEqual(result, 15)
+
+if __name__ == "__main_":
+    unittest.main()
